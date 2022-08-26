53d0ca22bd74b1d16e9a1b9f0299f09ea48d328a
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2022-08-26 19:36:57 +0300

    Added Cancel button in ProfileHost view

--- Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -11,6 +11,12 @@ struct ProfileHost: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             HStack {
+                if editMode?.wrappedValue == .active {
+                    Button("Cancel", role: .cancel) {
+                        draftProfile = modelData.profile
+                        editMode?.animation().wrappedValue = .inactive
+                    }
+                }
                 Spacer()
                 EditButton()
             }
@@ -18,6 +24,12 @@ struct ProfileHost: View {
                 ProfileSummary(profile: modelData.profile)
             } else {
                 ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear {
+                        modelData.profile = draftProfile
+                    }
             }
         }
         .padding()
